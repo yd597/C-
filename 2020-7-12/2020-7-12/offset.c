90975123b3fcbb8e421fc39a0489b213f08464ba
jingyanx1 <937053878@qq.com>
2020-09-13 19:12:48 +0800

    code

---
+++ 2020-7-12/2020-7-12/offset.c
@@ -0,0 +1,26 @@
+//#include <stdio.h>
+//#include <windows.h>
+////# define (StructType, MemberName£©
+//# define  CHANGE(a) ((a&0x55555555<<1£©|£¨a&0xaaaaaaaa>>1£©)
+//int main()
+//{
+//	int a = 5;
+//	printf("%d\n", CHANGE(a));
+//	//printf("%d\n", CHANGE(a));
+//	system("pause");
+//	return 0;
+//}
+////#define offsetof(StructType, MemberName) (size_t)&(((StructType *)0)->MemberName)
+
+#include <stdio.h>
+#include  <windows.h>
+#define MAX  1000
+#define reg  register
+#define do_forever  for(;;)
+#define CASE 
+int main()
+{
+	system("pause");
+	return 0;
+
+}
